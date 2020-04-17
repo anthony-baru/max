86f04d91ac63376bed07e6860a3b26852578df7b
anthony-baru <anthonybaru@gmail.com>
2020-04-17 08:05:32 +0300

    added products model

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-const rootDir = require('./util/path')
+const errorController = require('./controllers/error')
 // routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -11,9 +11,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found!' })
-});
+//redering errors like 404
+app.use(errorController.get404);
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 const port = process.env.port || 3000;
