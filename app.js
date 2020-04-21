46e23d3103ff5784b4d0a4ac394bd9d4f55636b5
anthony-baru <anthonybaru@gmail.com>
2020-04-21 11:09:58 +0300

    add to cart error

--- app.js
+++ app.js
@@ -49,8 +49,8 @@ Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
-sequelize.
-    sync({ force: true })
+sequelize
+    .sync()
     // .sync()
     .then(result => {
         return User.findOne({
@@ -66,8 +66,11 @@ sequelize.
         return user;
     })
     .then(user => {
+        // console.log(user);
+        return user.createCart();
+    })
+    .then(cart => {
         app.listen(port);
-        console.log(user);
     })
     .catch(err => {
         console.log(err);
