0ad61dbabe7c9e671498c3a22da9a258b36e8a65
anthony-baru <anthonybaru@gmail.com>
2020-05-04 12:52:35 +0300

    route with validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const sendMail = require('../util/mail.config');
 const crypto = require('crypto');
+const { validationResult } = require('express-validator');
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
     if (message.length > 0) {
@@ -67,6 +68,18 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array())
+        return res.status(422).render('auth/signup', {
+            path: '/signup',
+            pageTitle: 'Signup',
+            isAuthenticated: false,
+            csrfToken: req.csrfToken(),
+            errorMessage: errors.array()[0].msg
+
+        });
+    }
     User.findOne({ email: email })
         .then(user => {
             if (user) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,30 @@
 const express = require('express');
-
+const { check, body } = require('express-validator')
 const authController = require('../controllers/auth');
 
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
-router.get('/signup', authController.getSignup);
-
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.get('/signup', authController.getSignup);
+
+router.post('/signup', [
+    check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email!')
+        .custom((value, { req }) => {
+            if (value == 'anthonybaru@gmail.com') {
+                throw new Error('This email is forbidden.')
+            }
+            return true;
+        }),
+    body('password')
+        .isLength({ min: 5 }).withMessage('Must be min 5 characters')
+        .isAlphanumeric().withMessage('Must be alphanumeric')
+],
+    authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
